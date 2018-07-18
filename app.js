f999a7e097cdb230ee4ae5b655e39a3faae9b9b6
Benson Zheng <benson515@sbcglobal.net>
2018-07-17 21:17:50 -0700

    ejs routes setup

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express"),
 	app = express()
 const PORT_NUM = 3000;
 var bodyParser = require("body-parser")
-
+var indexRoute = require("./routes/index")
 
 app.use(express.static(__dirname + '/public'));
 
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 
 //MAIN
 app.get("/", function(req, res){
-	res.send("HEllo world")
+	res.render("index")
 })
 
 app.listen(PORT_NUM, function(){
