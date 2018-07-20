a24d9fb87f940c3d7cc48d92a8b18a2f9fcf0b33
Benson Zheng <benson515@sbcglobal.net>
2018-07-19 23:05:47 -0700

    dummy date in db ready to test

--- app.js
+++ app.js
@@ -2,7 +2,13 @@ var express = require("express"),
 	app = express()
 var bodyParser 	= require("body-parser"), 
     mongoose	= require("mongoose"),
-    recipeRoute	= require("./routes/recipe")
+    recipeRoute	= require("./routes/recipe"),
+    fs			= require("fs"),
+    parse 		= require('csv-parse'),
+    async 		= require('async');
+    Recipe	= require("./models/recipesSchema")
+
+
 
 app.use(express.static(__dirname + '/public'));
 const PORT_NUM = 3000;
@@ -39,8 +45,54 @@ app.use(bodyParser.urlencoded({extended: true}));
 // 	res.render("index", {testArray: testArr})
 // })
 
+
+// var inputFile='drinks_test.csv';
+// var parser = parse({delimiter: ','}, function (err, data) {
+//   async.eachSeries(data, function (line, callback) {
+//     // do something with the line
+
+// 		console.log(line)
+// 		var newRecipe = {
+// 			date: line[0], 
+// 			title: line[1], 
+// 			img_source: line[2],
+// 			ingredients: line[3],
+// 			instructions: line[4],
+// 			rating: line[5],
+// 			recipe_source: line[6]
+// 		};
+// 		Recipe.create(newRecipe, function(err, recipe){
+// 			if(err){
+// 				console.log(err)
+// 			}else{
+// 				console.log("saved to db")
+// 			}
+// 		})
+//     // doSomething(line).then(function() {
+//     //   // when processing finishes invoke the callback to move to the next one
+//       callback();
+//     // });
+//   })
+// });
+// fs.createReadStream(inputFile).pipe(parser);
+
+
+
+
 app.get("/", function(req, res){
 	res.render("index")
+})
+
+app.get("/drinks", function(req, res){
+	Recipe.find({}, function(err, foundRecipe){
+		if(err){
+			console.log(err)
+			res.redirect("/")
+		}else{
+			console.log(foundRecipe)
+			res.send({foundRecipe})
+		}
+	})
 })
 
 // app.get("/:", function(req, res){
