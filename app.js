c722d8bd227f6804d419d242fb29547c53137a8a
Benson Zheng <benson515@sbcglobal.net>
2018-08-15 18:08:33 -0700

    files uploadable

--- app.js
+++ app.js
@@ -11,23 +11,42 @@ var bodyParser 		= require("body-parser"),
     //authentication
     passport		= require("passport"),
     localStrategy 	= require("passport-local"),
-    authenticateRoute = require("./routes/authenticateRoute")
+    authenticateRoute = require("./routes/authenticateRoute"),
+    multer          = require("multer")
 
 
 
+//Sset static routes
 app.use(express.static('public'));
 app.use(express.static(__dirname + "/views"))
-
 const PORT_NUM = 3000;
 
-app.set("view engine", "ejs");
+// //Set View engine
+// app.set("view engine", "ejs");
 
+// //Set Storage Engine
+// const storage = multer.diskStorage({
+//     destination: "./public/testStorage/",
+//     filename: function(req, file, cb){
+//         //callback with fieldname concat file extension
+//         cb(null, file.fieldname + path.extname(file.originalname))
+//     }
+// });
+
+// //Init Upload variable
+// const upload = multer({
+//     storage: storage
+// }).single("fileName")
+
+
+
+//Connect to mongoose
 mongoose.connect('mongodb://localhost:27017/notMyRecipes', {useNewUrlParser: true})
 
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-// all things user auth
+// All things user auth
 app.use(require("express-session")({
 	secret:"supersecret",
 	resave:false,
@@ -41,52 +60,7 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 //MAIN
-pushtodb()
-
-
-
-// push to db
-// var inputFile='drinks_test.csv';
-// var inputFile = "dessert_recipes.csv";
-// var parser = parse({delimiter: ','}, function (err, data) {
-// 	// console.log(data[1][3].split("\r\n"))
-// 	// console.log(data[1]);
-// 	// console.log(data.length)
-// 	// category,title,image_link,ingredients,instructions,recipe_group,recipe_url
-// 	// 0			1     2 		3 			4 				5 			6
-// 	// console.log(data[2])
-//   async.eachSeries(data, function (line, callback) {
-//     // do something with the line
-
-// 		// console.log(line)
-// 		var newRecipe = {
-// 			category: line[0], 
-// 			title: line[1],
-// 			image_link: line[2], 
-// 			ingredients: line[3].split("\n"),
-// 			instructions: line[4].split("\n"),
-// 			recipe_group: line[5],
-// 			recipe_url: line[6]
-// 		};
-
-// 		Recipe.create(newRecipe, function(err, recipe){
-// 			if(err){
-// 				console.log(err)
-// 			}else{
-// 				console.log("saved to db")
-// 			}
-// 		})
-// 		// save to mongo
-//     // doSomething(line).then(function() {
-//     //   // when processing finishes invoke the callback to move to the next one
-//       callback();
-//     // });
-//   })
-// });
-// fs.createReadStream(inputFile).pipe(parser);
-
-
-
+// pushtodb()
 
 app.get("/", function(req, res){
 	// console.log(req.user)
